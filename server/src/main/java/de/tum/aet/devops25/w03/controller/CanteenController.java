fee07d85a1d40f2aa77f9f25866d424b3d00a67e
florian <ge24tuj@mytum.de>
2025-05-09 13:22:39 +0200

    implemented changes from lecture

--- server/src/main/java/de/tum/aet/devops25/w03/controller/CanteenController.java
+++ server/src/main/java/de/tum/aet/devops25/w03/controller/CanteenController.java
@@ -26,6 +26,16 @@ public class CanteenController {
     @GetMapping("/{canteenName}/today")
     public ResponseEntity<List<Dish>> getTodayMeals(@PathVariable String canteenName) {
         // TODO: Implement the logic to fetch today's meals for the specified canteen using the CanteenService
-        return ResponseEntity.ok(List.of());
+        //return ResponseEntity.ok(List.of());
+
+        /**
+         * Get today's meals for a specific canteen
+         * @param canteenName the ID of the canteen (e.g., "mensa-garching")
+         * @return list of dishes available today at the specified canteen
+         */
+        
+        List<Dish> todayMeals = canteenService.getTodayMeals(canteenName); 
+        return ResponseEntity.ok(todayMeals);
+        
     }
 }
